2c19bac25b4bef19cf7034eb2f67417f8264caa4
victodomvar <victor.dominguez506@comunidadunir.net>
2025-03-20 09:18:18 +0100

    Initial commit

---
+++ src/main/java/com/energy/DataIngestionService/DataIngestionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.energy.DataIngestionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIngestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataIngestionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/energy/DataIngestionService/DataIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energy.DataIngestionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
